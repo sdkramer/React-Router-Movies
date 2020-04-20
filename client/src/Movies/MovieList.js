41f9e68e0b2afb0c9324a0849c073747a7c5ad7a
Sean Kramer <seandominickramer@gmail.com>
2020-04-20 16:43:05 -0400

    Add 2 routes to app

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,31 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const MovieList = props => {
-  const [movies, setMovies] = useState([])
+const MovieList = (props) => {
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
+        .get("http://localhost:5000/api/movies")
+        .then((response) => {
           setMovies(response.data);
         })
-        .catch(error => {
-          console.error('Server Error', error);
+        .catch((error) => {
+          console.error("Server Error", error);
         });
-    }
-    
+    };
+
     getMovies();
   }, []);
-  
+
   return (
     <div className="movie-list">
-      {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+      {movies.map((movie) => (
+        <>
+          <MovieDetails key={movie.id} movie={movie} />
+          <Link></Link>
+        </>
       ))}
     </div>
   );
-}
+};
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
@@ -40,7 +43,7 @@ function MovieDetails({ movie }) {
       </div>
       <h3>Actors</h3>
 
-      {stars.map(star => (
+      {stars.map((star) => (
         <div key={star} className="movie-star">
           {star}
         </div>
