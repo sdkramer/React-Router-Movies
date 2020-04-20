a0dffcb7668ba67ea129daed7f9c7c87007b35fe
Sean Kramer <seandominickramer@gmail.com>
2020-04-20 17:26:14 -0400

    Make movie card a link

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 import axios from "axios";
 
 const MovieList = (props) => {
@@ -22,8 +23,10 @@ const MovieList = (props) => {
     <div className="movie-list">
       {movies.map((movie) => (
         <>
+
+          <Link to={`/movies/${movie.id}`}>
           <MovieDetails key={movie.id} movie={movie} />
-          <Link></Link>
+          </Link>
         </>
       ))}
     </div>
